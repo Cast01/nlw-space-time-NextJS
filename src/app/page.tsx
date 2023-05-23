05dd0a74f9151f6bd7870f35693143057a2d9f19
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-05-23 18:19:07 -0300

    update: get memories list from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import { EmptyMemories } from '@/components/Memoriesteste'
+import { api } from '@/lib/apiteste'
+import { cookies } from 'next/dist/client/components/headers'
 
-export default function Home() {
-    return <EmptyMemories />
+export default async function Home() {
+    const isAuthenticated = !!cookies().get('token')?.value
+
+    if (!isAuthenticated) {
+        return <EmptyMemories />
+    }
+
+    const token = cookies().get('token')?.value
+    const response = await api.get('/memories', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+
+    const memoriesList = response.data
+
+    return <div>{JSON.stringify(memoriesList)}</div>
 }
